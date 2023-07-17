4dbea1a64362393e2931b9c3f04cb058c3fd91a2
Jorge Monterroso <jorge.monterroso@ecosabarl.com>
2023-07-17 07:40:18 -0600

    Agregado de flowbite, preact, header y 404

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-	darkMode: ['class', '[data-mode="dark"]'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	darkMode: 'class',
+	theme: {},
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
